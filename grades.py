110f05f3d91942a0d8ac878ef4594a120dff1770
Will Crichton <willcrichton@cmu.edu>
2015-01-05 14:58:09 -0600

    Updated grades.py with SIO changes

--- grades.py
+++ grades.py
@@ -2,7 +2,7 @@ from pyquery import PyQuery as pq
 from auth import authenticate
 from datetime import datetime
 from urllib import urlencode
-from icalendar import Calendar, Event, UTC
+from icalendar import Calendar, UTC
 import re
 import json
 
@@ -28,7 +28,7 @@ def get_final_grades():
 
     # get page for given major
     classes = s.get('https://enr-apps.as.cmu.edu/audit/audit?' + urlencode(params)).content
-    
+
     # take grades from <pre>s in the page
     d = pq(classes)
     courses = {}
@@ -40,12 +40,13 @@ def get_final_grades():
                 course = matches.group(1)
                 grade = matches.group(2)
                 courses[course] = grade
-                    
+
     return courses
 
+# TODO: fix autolab
 def get_autolab_grades():
     s = authenticate('https://autolab.cs.cmu.edu')
-    
+
     main = s.get('https://autolab.cs.cmu.edu').content
     d = pq(main)
     current_courses = d('#content > ul > li > a')
@@ -70,7 +71,7 @@ def get_autolab_grades():
                 total = float(matches.group(3))
 
                 grades[course_name][name] = [score, total]
-                
+
 
     return grades
 
@@ -83,12 +84,12 @@ def get_sio():
     s.headers['Content-Type'] = 'text/x-gwt-rpc; charset=UTF-8'
 
     siojs = s.get('https://s3.as.cmu.edu/sio/sio/sio.nocache.js').content
-    permutation = re.search("Ub='([^']+)'", siojs).group(1)
+    permutation = re.search("Rb='([^']+)'", siojs).group(1)
 
     page_name = 'https://s3.as.cmu.edu/sio/sio/%s.cache.html' % (permutation)
     cachehtml = s.get(page_name).content
 
-    # to successfully do RPC with SIO, you have to find the correct keys 
+    # to successfully do RPC with SIO, you have to find the correct keys
     # for each different kind of RPC you're doing and send them with the request
     def get_key(key):
         var_name = re.search("'%s',(\w+)," % key, cachehtml).group(1)
@@ -100,23 +101,25 @@ def get_sio():
     # GWT returns something that's _almost_ JSON but not quite
     def parse_gwt(gwt_response):
         return json.loads(gwt_response.replace("'", '"').replace("\\", "\\\\")[4:])
-    
+
     return_data = {}
 
     # info in user context: full name, major/school
-    s.post('https://s3.as.cmu.edu/sio/sio/userContext.rpc', 
+    s.post('https://s3.as.cmu.edu/sio/sio/userContext.rpc',
            data=('7|0|4|https://s3.as.cmu.edu/sio/sio/|%s|edu.cmu.s3.ui.common.client.serverproxy.user.UserContextService|initUserContext|1|2|3|4|0|' % context_key))
 
     # get mailbox/smc
     gwt_response =  s.post('https://s3.as.cmu.edu/sio/sio/bioinfo.rpc',
                            data=('7|0|4|https://s3.as.cmu.edu/sio/sio/|%s|edu.cmu.s3.ui.sio.student.client.serverproxy.bio.StudentBioService|fetchStudentSMCBoxInfo|1|2|3|4|0|' % content_key)).content
-    sio_json = parse_gwt(gwt_response) 
+    sio_json = parse_gwt(gwt_response)
 
     return_data['smc'] = sio_json[5][2]
     return_data['mailbox_combo'] = sio_json[5][1]
 
     # get schedule
-    cal = Calendar.from_string(s.get('https://s3.as.cmu.edu/sio/export/schedule/S14_semester.ics?semester=S14').content)
+    now = datetime.now()
+    currSemester = ('F' if now.month > 6 else 'S') + str(now.year % 100)
+    cal = Calendar.from_string(s.get('https://s3.as.cmu.edu/sio/secure/export/schedule/%s_semester.ics?semester=%s' % (currSemester, currSemester)).content)
     day_map = {'MO': 1, 'TU': 2, 'WE': 3, 'TH': 4, 'FR': 5}
     return_data['schedule'] = []
     for event in cal.walk():
@@ -132,7 +135,7 @@ def get_sio():
 
     return return_data
 
-
+# TODO: fix blackboard
 def get_blackboard_grades():
     ''' returns all your grades from the current semester
     returns a json of courses mapping to a json of homeworks mapping to an array of [score, total]
@@ -155,20 +158,20 @@ def get_blackboard_grades():
         raise Exception('blackboard connection failed')
 
     grades = {}
-    now = datetime.now()
 
+    now = datetime.now()
     currSemester = ('F' if now.month > 6 else 'S') + str(now.year % 100)
 
     for course_id, course in bbGrades['sv_extras']['sx_filters'][0]['choices'].iteritems():
 
         # we rely on blackboard's naming convention that all courses are of the pattern [SEASON][YEAR]-[COURSE NAME]
         # also, :3
-        if course[:3] != currSemester: continue
+        #if course[:3] != currSemester: continue
 
         # get the HTML page for the specific set of grades we want
         grades[course] = {}
         html = s.get('https://blackboard.andrew.cmu.edu/webapps/bb-mygrades-BBLEARN/myGrades?course_id=%s&stream_name=mygrades' % course_id).content
-        
+
         # use pyquery (like jQuery) to extract grades
         d = pq(html)
         for homework in d('.grade-item'):
